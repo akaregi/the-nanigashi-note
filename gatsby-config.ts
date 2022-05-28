d2beb082222e00e47b562516e7c0114b3765ffb8
akaregi <verdigris@azure.icu>
2022-05-29 03:57:24 +0900

    chore: support graphql
    Signed-off-by: akaregi <verdigris@azure.icu>

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,16 +3,25 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `The Nanigashi Note`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", "gatsby-plugin-mdx", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+  plugins: [
+    "gatsby-plugin-postcss",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-mdx",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
     },
-    __key: "pages"
-  }]
+  ],
+  flags: {
+    GRAPHQL_TYPEGEN: true,
+  },
 };
 
 export default config;
