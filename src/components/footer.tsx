0f726436840bcb63063cdcd506975471a62d0928
Mizuho SHINOHARA <verdigris@azure.icu>
2022-06-13 11:32:58 +0000

    change: refactor
    Signed-off-by: GitHub <noreply@github.com>

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,15 +2,19 @@ import React from "react";
 
 import { constants } from "./constants";
 
-const style = {
+const footerStyle = {
+  margin: "5rem 0",
+};
+
+const memoStyle = {
   color: constants.TEXT_BG,
 };
 
 export function Footer() {
   return (
-    <footer>
+    <footer style={footerStyle}>
       <p>(C) 2022 nanigashi. All rights reserved.</p>
-      <p style={style}>This site is built with 💖 and 💯 techlonogies.</p>
+      <p style={memoStyle}>This site is built with 💖 and 💯 techlonogies.</p>
     </footer>
   );
 }
